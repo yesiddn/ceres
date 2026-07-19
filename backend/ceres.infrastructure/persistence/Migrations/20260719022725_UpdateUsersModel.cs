48b8c538b49c3ee91f7af6b48fd9b609ff5ed8a0
yesiddn <yesidrodriguez305@gmail.com>
2026-07-18 21:29:20 -0500

    Add users migration

---
+++ backend/ceres.infrastructure/persistence/Migrations/20260719022725_UpdateUsersModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ceres.infrastructure.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsersModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "identity",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                schema: "identity",
+                table: "Users");
+        }
+    }
+}
