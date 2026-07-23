7a54f424ccfa5b5c278292cb5692067211253718
yesiddn <yesidrodriguez305@gmail.com>
2026-07-22 20:32:18 -0500

    Add application and infrastructure services for authentication

--- backend/ceres.api/Extensions/ServiceCollectionExtensions.cs
+++ backend/ceres.api/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,9 @@
 using ceres.application.HealthCheck.Interfaces;
 using ceres.application.HealthCheck.Services;
+using ceres.application.Identity.Interfaces;
+using ceres.application.Identity.Services;
+using ceres.domain.Identity.Entities;
+using ceres.infrastructure.Repositories.Identity;
 
 namespace ceres.api.Extensions;
 
@@ -8,6 +12,14 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IHealthCheckService, HealthCheckService>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IAuthService, AuthService>();
+        return services;
+    }
+
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
         return services;
     }
 }
