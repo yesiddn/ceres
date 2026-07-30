b403945a871a644e62980ba436379778f8916f59
yesiddn <yesidrodriguez305@gmail.com>
2026-07-30 18:40:32 -0500

    update loginSchema

--- frontend/src/modules/auth/schemas/loginSchema.ts
+++ frontend/src/modules/auth/schemas/loginSchema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  email: z.email().min(1, "El correo es obligatorio"),
+  email: z.email("Ingresa un correo válido").min(1, "El correo es obligatorio"),
   password: z.string().min(1, "La contraseña es obligatoria"),
 });
 
