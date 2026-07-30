918b381fa24f92ffdebf09be6b7c7108837e60cf
yesiddn <yesidrodriguez305@gmail.com>
2026-07-29 21:58:02 -0500

    add login interfaces

---
+++ frontend/src/modules/auth/types/auth.ts
@@ -0,0 +1,12 @@
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  accessToken: string;
+  user: {
+    id: string;
+    email: string;
+  };
+}
