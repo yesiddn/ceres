7d9898e250ad6bfa495a3c66bb98270b83d4a80e
yesiddn <yesidrodriguez305@gmail.com>
2026-07-30 19:50:26 -0500

    add aliases to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
