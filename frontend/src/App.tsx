dbde4e684cef62f2c939e8a647c93fd818bddf0d
yesiddn <yesidrodriguez305@gmail.com>
2026-07-30 21:15:54 -0500

    update App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,13 @@
-import { useEffect, useState } from "react";
+import { RouterProvider } from "react-router/dom";
 import "./App.css";
-
-interface HealthResponse {
-  status: string;
-  dataBaseStatus: string;
-  timestamp: string;
-}
-
-async function getHealth(signal: AbortSignal) {
-  const res = await fetch("/api/health", { signal });
-  if (!res.ok) {
-    throw new Error(`Health check failed with status ${res.status}`);
-  }
-  const data: HealthResponse = await res.json();
-  return data;
-}
+import { AppProviders } from "./app/providers/AppProviders";
+import { router } from "./app/router/router";
 
 function App() {
-  const [health, setHealth] = useState<HealthResponse | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    const fetchHealth = async () => {
-      try {
-        const data = await getHealth(controller.signal);
-        setHealth(data);
-        setError(null);
-      } catch (err) {
-        if (err instanceof Error && err.name === "AbortError") {
-          console.log("Fetch aborted");
-          return; // cancelación intencional, no es un error real
-        }
-        setError("No se pudo conectar con el servidor");
-        console.error("Error fetching health check:", err);
-      }
-    };
-
-    fetchHealth();
-
-    return () => controller.abort();
-  }, []);
-
   return (
-    <>
-      <h1>Connection status</h1>
-      {error ? (
-        <p style={{ color: "red" }}>{error}</p>
-      ) : health ? (
-        <div>
-          <p>Status: {health.status}</p>
-          <p>Database Status: {health.dataBaseStatus}</p>
-          <p>Timestamp: {health.timestamp}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </>
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   );
 }
 
