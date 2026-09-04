63d70458f4fc8853f2f6031fc52c451197e3529f
yesiddn <yesidrodriguez305@gmail.com>
2026-09-03 19:33:14 -0500

    add IExerciseRepository to ServiceCollection for dependency injection

--- backend/ceres.api/Extensions/ServiceCollectionExtensions.cs
+++ backend/ceres.api/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
+using ceres.application.Gym.Exercises.Interfaces;
 using ceres.application.HealthCheck.Interfaces;
 using ceres.application.HealthCheck.Services;
 using ceres.application.Identity.Interfaces;
 using ceres.application.Identity.Services;
 using ceres.infrastructure.HealthCheck;
+using ceres.infrastructure.Repositories.Gym;
 using ceres.infrastructure.Repositories.Identity;
 
 namespace ceres.api.Extensions;
@@ -22,6 +24,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IDatabaseHealthCheck, DatabaseHealthCheck>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IExerciseRepository, ExerciseRepository>();
         return services;
     }
 }
